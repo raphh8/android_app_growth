73cf9b981c2a0442c43bb12ee3bb9949ed1fa7d8
raphh8 <149191554+raphh8@users.noreply.github.com>
2025-12-11 23:00:28 +0100

    data class task implemented

---
+++ app/src/main/java/com/projet/mobile/growth/list/Task.kt
@@ -0,0 +1,3 @@
+package com.projet.mobile.growth.list
+
+data class Task(val id: String, var title: String, var description: String = "You can add your own description!")
--- app/src/main/java/com/projet/mobile/growth/list/TaskListAdapter.kt
+++ app/src/main/java/com/projet/mobile/growth/list/TaskListAdapter.kt
@@ -1,16 +1,44 @@
 package com.projet.mobile.growth.list
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.projet.mobile.growth.R
 
 class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
 
-    var currentList: List<String> = emptyList()
+    private val taskList = listOf(
+        Task(id = "id_1", title = "Task 1", description = "description 1"),
+        Task(id = "id_2", title = "Task 2"),
+        Task(id = "id_3", title = "Task 3")
+    )
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TaskViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView)
+    }
 
-    // on utilise `inner` ici afin d'avoir accès aux propriétés de l'adapter directement
+    override fun onBindViewHolder(
+        holder: TaskViewHolder,
+        position: Int
+    ) {
+        holder.bind(taskList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return taskList.size
+    }
+
     inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(taskTitle: String) {
-            // on affichera les données ici
+        fun bind(task: Task) {
+            val iTitle = itemView.findViewById<TextView>(R.id.task_title)
+            val iDesc= itemView.findViewById<TextView>(R.id.task_desc)
+            iTitle.text = task.title
+            iDesc.text = task.description
         }
     }
 }
