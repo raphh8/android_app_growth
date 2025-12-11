8ffeb4de41c40a272e2b7310476f39027977f72a
raphh8 <149191554+raphh8@users.noreply.github.com>
2025-12-11 23:28:42 +0100

    functionality: add task

--- app/src/main/java/com/projet/mobile/growth/list/TaskListFragment.kt
+++ app/src/main/java/com/projet/mobile/growth/list/TaskListFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.projet.mobile.growth.R
+import java.util.UUID
 
 class TaskListFragment : Fragment() {
 
-    private val taskList = listOf(
+    private var taskList = listOf(
         Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3")
@@ -30,5 +32,16 @@ class TaskListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler)
         recyclerView.adapter = adapter
+        val addTask = view.findViewById<FloatingActionButton>(R.id.add_task)
+        addTask.setOnClickListener {
+            val newTask = Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
+            taskList = taskList + newTask
+            refreshAdapter(adapter, taskList)
+        }
+    }
+
+    fun refreshAdapter(a: TaskListAdapter, newList: List<Task>) {
+        a.currentList = newList
+        a.notifyDataSetChanged()
     }
 }
