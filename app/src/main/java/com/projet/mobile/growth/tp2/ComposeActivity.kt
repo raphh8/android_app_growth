ab8a0776919b771b3680149b468d91f0088b0e07
raphh8 <149191554+raphh8@users.noreply.github.com>
2025-12-15 11:52:52 +0100

    compose using remember

--- app/src/main/java/com/projet/mobile/growth/tp2/ComposeActivity.kt
+++ app/src/main/java/com/projet/mobile/growth/tp2/ComposeActivity.kt
@@ -7,19 +7,27 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.projet.mobile.growth.list.Task
 import com.projet.mobile.growth.tp2.ui.theme.GrowthTheme
 import com.projet.mobile.growth.ui.TodoTheme
+import java.util.UUID
 
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +43,11 @@ class ComposeActivity : ComponentActivity() {
 
 @Composable
 fun ListScreen(modifier: Modifier = Modifier) {
+    var items by remember { mutableStateOf(List<Task>(100) {
+        Task(id = UUID.randomUUID().toString(),
+        title ="Item #$it")
+    }) }
+
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = modifier
@@ -42,11 +55,11 @@ fun ListScreen(modifier: Modifier = Modifier) {
             .padding(16.dp)
             .background(MaterialTheme.colorScheme.background)
     ) {
-        items(100) { index ->
-            Text(
-                text = "Item #$index",
-                color = MaterialTheme.colorScheme.onBackground
-            )
+        items(items) { task ->
+            Column {
+                Text(text = task.title, color = MaterialTheme.colorScheme.primary)
+                Text(text = task.description, color = MaterialTheme.colorScheme.secondary)
+            }
         }
     }
 }
