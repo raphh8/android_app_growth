b7e5654443f6e8c0504f03bfd5b65e46012f9194
raphh8 <149191554+raphh8@users.noreply.github.com>
2025-12-15 12:10:26 +0100

    add elements

--- app/src/main/java/com/projet/mobile/growth/tp2/ComposeActivity.kt
+++ app/src/main/java/com/projet/mobile/growth/tp2/ComposeActivity.kt
@@ -12,14 +12,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,6 +36,8 @@ import androidx.compose.ui.unit.dp
 import com.projet.mobile.growth.list.Task
 import com.projet.mobile.growth.tp2.ui.theme.GrowthTheme
 import com.projet.mobile.growth.ui.TodoTheme
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import java.util.UUID
 
 class ComposeActivity : ComponentActivity() {
@@ -41,24 +52,43 @@ class ComposeActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListScreen(modifier: Modifier = Modifier) {
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
     var items by remember { mutableStateOf(List<Task>(100) {
         Task(id = UUID.randomUUID().toString(),
         title ="Item #$it")
     }) }
 
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .background(MaterialTheme.colorScheme.background)
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopAppBar(title = { Text("To Do List") }) },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                val newItem = Task(id = UUID.randomUUID().toString(), title = "Item #${items.size}")
+                items = items + newItem
+                coroutineScope.launch { listState.animateScrollToItem(index = items.size - 1) }
+            }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+            }
+        }
     ) {
-        items(items) { task ->
-            Column {
-                Text(text = task.title, color = MaterialTheme.colorScheme.primary)
-                Text(text = task.description, color = MaterialTheme.colorScheme.secondary)
+        LazyColumn(
+            state = listState,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            items(items) { task ->
+                Column {
+                    Text(text = task.title, color = MaterialTheme.colorScheme.primary)
+                    Text(text = task.description, color = MaterialTheme.colorScheme.secondary)
+                }
             }
         }
     }
